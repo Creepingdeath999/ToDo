e6d1e1ced148926e34c997b8d2ffc99e3db57713
Creepingdeath999 <mtchedlidze.elguja@atsu.edu.ge>
2021-03-18 14:33:35 +0400

    .

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-const sideMenu=document.getElementById('side-menu');
-const myTasksH1=sideMenu.querySelector('.mytasks-h1');
+const sideMenu = document.getElementById("side-menu");
+const myTasksH1 = sideMenu.querySelector(".mytasks-h1");
 
 /*newtastk*/
-const addTask=document.getElementById('add-task');
-const addTaskBtn=document.getElementById('save');
-const cancel=addTaskBtn.previousElementSibling;
+const addTask = document.getElementById("add-task");
+const addTaskBtn = document.getElementById("save");
+const cancel = addTaskBtn.previousElementSibling;
+const trashBtn=document.querySelector('#trash-btn');
+const trashContainer=document.getElementById('trash');
+const completedTask = document.querySelector(".completed-tasks");
+let taskContainer = document.querySelector(".active-tasks");
+const titleInpt = document.getElementById("task-title");
+const descr = titleInpt.nextElementSibling;
+const newTaskBtn = document.getElementById("new-task-btn");
+myTasksH1.addEventListener("click", () => {
+  document.querySelector("ul").classList.toggle("mytasks-visible");
+  myTasksH1
+    .querySelector("h1")
+    .nextElementSibling.classList.toggle("arrow-rotated");
+});
 
-const completedTask=document.querySelector('.completed-tasks');
-let taskContainer=document.querySelector('.active-tasks');
-const titleInpt=document.getElementById('task-title');
-const descr=titleInpt.nextElementSibling;
-
-const newTaskBtn=document.getElementById('new-task-btn')
-myTasksH1.addEventListener('click', ()=>{
-    document.querySelector('ul').classList.toggle('mytasks-visible');
-    myTasksH1.querySelector('h1').nextElementSibling.classList.toggle('arrow-rotated');
-})
-
-
-
-function newTask(){
-     let task=document.createElement('div');
-     task.classList.add('active-task');
-     task.innerHTML=`
+function newTask() {
+  let task = document.createElement("div");
+  task.classList.add("active-task");
+  task.innerHTML = `
     
      <div class="circle"></div>
      <div class="task-content">
@@ -34,63 +34,66 @@ function newTask(){
            <span>${getDate()}</span>
        </div>
        </div>
-     `
-    if(descr.value.trim()!=="" && titleInpt.value.trim()!==""){
-        taskContainer.append(task);
-        addTask.classList.toggle('visible');
-
-    }else{
-        return;
-    }
+       
+     `;
+  if (descr.value.trim() !== "" && titleInpt.value.trim() !== "") {
+    taskContainer.append(task);
+    addTask.classList.toggle("visible");
+  } else {
+    return;
+  }
 }
 
-function getDate(){
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-    let current=new Date();
-    let hr=current.getHours();
-    let min=current.getMinutes();
+function getDate() {
+  let current = new Date();
+  let hr = current.getHours();
+  let min = current.getMinutes();
 
-
-    return `${hr}:${min}`;
+  return `${hr}:${min}`;
 }
 
 /*event listeners*/
 
-addTaskBtn.addEventListener('click', newTask);
-newTaskBtn.addEventListener('click',()=>{
-    addTask.classList.toggle('visible');
+addTaskBtn.addEventListener("click", newTask);
+newTaskBtn.addEventListener("click", () => {
+  addTask.classList.toggle("visible");
 });
-cancel.addEventListener('click', ()=>{
-    addTask.classList.toggle('visible');
+cancel.addEventListener("click", () => {
+  addTask.classList.toggle("visible");
 });
 
-taskContainer.addEventListener('click', e=>{
-    let circle=e.target;
-    if(circle.classList[0]==="circle"){
-        let task=circle.parentElement;
-        task.classList.add('completed-task');
-        completedTask.append(task);
-    }
+
+taskContainer.addEventListener("click", (e) => {
+  let circle = e.target;
+  if (circle.classList[0] === "circle") {
+    let task = circle.parentElement;
+    task.classList.add("completed-task");
+    completedTask.append(task);
+  }
 });
-completedTask.addEventListener('click', e=>{
-    let circle=e.target;
-    if(circle.classList[0]==="circle"){
-        let task=circle.parentElement;
- 
-       task.classList.add('active-task');
-        taskContainer.append(task);
-    
-    }
-})
+completedTask.addEventListener("click", (e) => {
+  let circle = e.target;
+  if (circle.classList[0] === "circle") {
+    let task = circle.parentElement;
 
-taskContainer.addEventListener('dblclick',e=>{
-    let task=e.target;
-        task.classList.add('completed-task');
-        completedTask.append(task);
-})
+    task.classList.add("active-task");
+    taskContainer.append(task);
+  }
+});
 
-completedTask.addEventListener('dblclick',e=>{
-    let task=e.target;
-        task.classList.add('active-task');
-        taskContainer.append(task);
+
+taskContainer.addEventListener("dblclick", (e) => {
+  let task = e.target;
+  task.classList.add("completed-task");
+  completedTask.append(task);
+});
+
+completedTask.addEventListener("dblclick", (e) => {
+  let task = e.target;
+  task.classList.add("active-task");
+  taskContainer.append(task);
+});
+
+trashBtn.addEventListener('click', ()=>{
+    trashContainer.classList.toggle('visible');
 })
